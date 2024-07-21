c0ed9cb6525e93b7628d9501084cb0a856d27fa6
Ajay <ajaybhagel1117@gmail.com>
2024-07-21 18:40:20 +0530

    one

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+function DescriptionBox() {
+  return (
+    <div className='descriptionbox'>
+     <div className='descriptionbox-navigator'>
+    <div className='descriptionbox-nav-box'>Description</div>
+    <div className='descriptionbox-nav-box fade'>Reviews(122)</div>
+     </div>
+     <div className='descriptionbox-description'>
+      <p>An e-commerce haven where convenience meets choice. Our website offers a seamless browsing experience, showcasing a myriad of products from electronics to fashion, all at your fingertips. With user-friendly navigation, secure transactions, and swift delivery, we redefine online shopping, ensuring satisfaction with every click.
+</p>
+<p>Explore our e-commerce oasis where innovation and customer care converge. From cutting-edge gadgets to timeless decor, our curated collection caters to diverse tastes. Enjoy hassle-free shopping with intuitive search options, detailed product descriptions, and reliable customer support. Embrace a digital marketplace that prioritizes your needs, delivering excellence with every purchase.</p>
+
+     </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
